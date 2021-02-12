0f0f24c11901a115d0d639cbc09b00e1f1f04f1d
David Tao <ruoyutao@gmail.com>
2021-02-12 14:46:26 +0800

    untested 'generalizations' complete

--- grl/envs/mountaincar.py
+++ grl/envs/mountaincar.py
@@ -1,48 +1,223 @@
 """
 Generalized Mountain Car. Based on specifications and code adapted from 
 https://www.cs.utexas.edu/users/pstone/Papers/bib2html-links/ADPRL11-shimon.pdf
+
+http://incompleteideas.net/sutton/MountainCar/MountainCar1.cp
+permalink: https://perma.cc/6Z2N-PFWC
+
+Modified from
+https://github.com/openai/gym/blob/master/gym/envs/classic_control/mountain_car.py
 """
 import math
+
 import numpy as np
 
-from gym.envs.classic_control import MountainCarEnv
+import gym
+from gym import spaces
+from gym.utils import seeding
 
 
-class GeneralizedMountainCarEnv(MountainCarEnv):
-    def __init__(self, goal_velocity=0, accel_bias_mean=1., accel_factor=0.001):
-        super(GeneralizedMountainCarEnv, self).__init__(goal_velocity)
-        self._accel_bias_mean = accel_bias_mean
-        self._accel_factor = accel_factor
+
+class MountainCarEnv(gym.Env):
+    """
+    Description:
+        The agent (a car) is started at the bottom of a valley. For any given
+        state the agent may choose to accelerate to the left, right or cease
+        any acceleration.
+    Source:
+        The environment appeared first in Andrew Moore's PhD Thesis (1990).
+    Observation:
+        Type: Box(2)
+        Num    Observation               Min            Max
+        0      Car Position              -1.2           0.6
+        1      Car Velocity              -0.07          0.07
+    Actions:
+        Type: Discrete(3)
+        Num    Action
+        0      Accelerate to the Left
+        1      Don't accelerate
+        2      Accelerate to the Right
+        Note: This does not affect the amount of velocity affected by the
+        gravitational pull acting on the car.
+    Reward:
+         Reward of 0 is awarded if the agent reached the flag (position = 0.5)
+         on top of the mountain.
+         Reward of -1 is awarded if the position of the agent is less than 0.5.
+    Starting State:
+         The position of the car is assigned a uniform random value in
+         [-0.6 , -0.4].
+         The starting velocity of the car is always assigned to 0.
+    Episode Termination:
+         The car position is more than 0.5
+         Episode length is greater than 200
+    """
+
+    metadata = {
+        'render.modes': ['human', 'rgb_array'],
+        'video.frames_per_second': 30
+    }
+
+    def __init__(self, goal_velocity=0, accel_bias_mean=1., p_offset=0., v_offset=0.,
+                 p_noise_divider=20., v_noise_divider=20.,
+                 gravity=0.0025, accel_factor=0.001):
+
+        self.accel_bias_mean = accel_bias_mean
+        self.p_offset = p_offset
+        self.v_offset = v_offset
+        self.p_noise_divider = p_noise_divider
+        self.v_noise_divider = v_noise_divider
+
+        self.min_position = -1.2
+        self.max_position = 0.6
+        self.max_speed = 0.07
+        self.goal_position = 0.5
+        self.goal_velocity = goal_velocity
+
+        self.accel_factor = accel_factor
+        self.gravity = gravity
+
+        self.low = np.array(
+            [self.min_position, -self.max_speed], dtype=np.float32
+        )
+        self.high = np.array(
+            [self.max_position, self.max_speed], dtype=np.float32
+        )
+
+        self.viewer = None
+
+        self.action_space = spaces.Discrete(3)
+        self.observation_space = spaces.Box(
+            self.low, self.high, dtype=np.float32
+        )
+
+        self.seed()
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def sample_force(self, mean):
+        adjusted_gaussian = self.np_random.normal(scale=0.05)
+        accel_noise = adjusted_gaussian + mean
+        varied_accel = self.accel_factor * accel_noise
+        return varied_accel
 
     def step(self, action: int):
-        adjusted_gaussian = np.random.normal(scale=0.05)
-        accel_noise = adjusted_gaussian + self._accel_bias_mean
-        varied_accel = self._accel_factor * accel_noise
+        assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
 
-        self.force = varied_accel
-        return super(GeneralizedMountainCarEnv, self).step(action)
-        # assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
-        # 
-        # position, velocity = self.state
-        # velocity += (action - 1) * self.force + math.cos(3 * position) * (-self.gravity)
-        # velocity = np.clip(velocity, -self.max_speed, self.max_speed)
-        # position += velocity
-        # position = np.clip(position, self.min_position, self.max_position)
-        # if (position == self.min_position and velocity < 0)::w
+        position, velocity = self.state
 
-        #     velocity = 0
-        # 
-        # done = bool(
-        #     position >= self.goal_position and velocity >= self.goal_velocity
-        # )
-        # reward = -1.0
-        # 
-        # self.state = (position, velocity)
-        # return np.array(self.state), reward, done, {}
+        varied_accel = self.sample_force(self.accel_bias_mean)
+
+        velocity += (action - 1) * varied_accel + math.cos(3 * position) * (-self.gravity)
+        velocity = np.clip(velocity, -self.max_speed, self.max_speed)
+        position += velocity
+        position = np.clip(position, self.min_position, self.max_position)
+        if (position == self.min_position and velocity < 0):
+            velocity = 0
+
+        done = bool(
+            position >= self.goal_position and velocity >= self.goal_velocity
+        )
+        reward = -1.0
+
+        self.state = (position, velocity)
+        observation = self.observation()
+        return np.array(observation), reward, done, {}
+
+    def make_noise(self, min_value: float, max_value: float) -> float:
+        noise = self.np_random.beta(5, 2) - 0.5
+        var_range = max_value - min_value
+        return noise * var_range
+
+    def observation(self) -> tuple:
+        noiseP = self.make_noise(self.min_position, self.max_position)
+        noiseV = self.make_noise(0, self.max_speed)
+
+        noiseP /= self.p_noise_divider
+        noiseV /= self.v_noise_divider
+
+        obs_pos = np.clip(self.state[0] + self.p_offset + noiseP, self.min_position, self.max_position)
+        obs_vel = np.clip(self.state[1] + self.v_offset + noiseV, 0, self.max_speed)
+
+        return (obs_pos, obs_vel)
 
 
-    def setAccelFactor(self, d: float):
-        self._accel_factor = d
+    def reset(self):
+        self.state = np.array([self.np_random.uniform(low=-0.6, high=-0.4), 0])
+        return np.array(self.state)
 
-    def setAccelBiasMean(self, d: float):
-        self._accel_bias_mean = d
+    def _height(self, xs):
+        return np.sin(3 * xs) * .45 + .55
+
+    def render(self, mode='human'):
+        screen_width = 600
+        screen_height = 400
+
+        world_width = self.max_position - self.min_position
+        scale = screen_width / world_width
+        carwidth = 40
+        carheight = 20
+
+        if self.viewer is None:
+            from gym.envs.classic_control import rendering
+            self.viewer = rendering.Viewer(screen_width, screen_height)
+            xs = np.linspace(self.min_position, self.max_position, 100)
+            ys = self._height(xs)
+            xys = list(zip((xs - self.min_position) * scale, ys * scale))
+
+            self.track = rendering.make_polyline(xys)
+            self.track.set_linewidth(4)
+            self.viewer.add_geom(self.track)
+
+            clearance = 10
+
+            l, r, t, b = -carwidth / 2, carwidth / 2, carheight, 0
+            car = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
+            car.add_attr(rendering.Transform(translation=(0, clearance)))
+            self.cartrans = rendering.Transform()
+            car.add_attr(self.cartrans)
+            self.viewer.add_geom(car)
+            frontwheel = rendering.make_circle(carheight / 2.5)
+            frontwheel.set_color(.5, .5, .5)
+            frontwheel.add_attr(
+                rendering.Transform(translation=(carwidth / 4, clearance))
+            )
+            frontwheel.add_attr(self.cartrans)
+            self.viewer.add_geom(frontwheel)
+            backwheel = rendering.make_circle(carheight / 2.5)
+            backwheel.add_attr(
+                rendering.Transform(translation=(-carwidth / 4, clearance))
+            )
+            backwheel.add_attr(self.cartrans)
+            backwheel.set_color(.5, .5, .5)
+            self.viewer.add_geom(backwheel)
+            flagx = (self.goal_position-self.min_position) * scale
+            flagy1 = self._height(self.goal_position) * scale
+            flagy2 = flagy1 + 50
+            flagpole = rendering.Line((flagx, flagy1), (flagx, flagy2))
+            self.viewer.add_geom(flagpole)
+            flag = rendering.FilledPolygon(
+                [(flagx, flagy2), (flagx, flagy2 - 10), (flagx + 25, flagy2 - 5)]
+            )
+            flag.set_color(.8, .8, 0)
+            self.viewer.add_geom(flag)
+
+        pos = self.state[0]
+        self.cartrans.set_translation(
+            (pos-self.min_position) * scale, self._height(pos) * scale
+        )
+        self.cartrans.set_rotation(math.cos(3 * pos))
+
+        return self.viewer.render(return_rgb_array=mode == 'rgb_array')
+
+    def get_keys_to_action(self):
+        # Control with left and right arrow keys.
+        return {(): 1, (276,): 0, (275,): 2, (275, 276): 1}
+
+    def close(self):
+        if self.viewer:
+            self.viewer.close()
+            self.viewer = None
+
+
