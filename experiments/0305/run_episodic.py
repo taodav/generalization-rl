28c7628e816a91387a93d34537112fd5539f06e0
movefast <liderek269@gmail.com>
2021-03-05 23:35:34 -0700

    added remote job func (wip)

---
+++ experiments/0305/run_episodic.py
@@ -0,0 +1,122 @@
+import copy
+import math
+import time
+from pathlib import Path
+
+import fire
+import gym
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn.functional as F
+from definitions import ROOT_DIR
+from fastprogress.fastprogress import master_bar, progress_bar
+from grl.agents.sarsa_lambda_tc import SarsaLambdaTCAgent
+from grl.agents.sarsa_nn import DQNAgent as SarsaDQNAgent
+from grl.agents.sarsa_nn_tc import DQNAgent as TCSarsaNNAgent
+from grl.agents.sarsa_tc import SarsaAgent as SarsaTCAgent
+from grl.envs.mountaincar import MountainCarEnv
+from grl.sampling import sample_mountaincar_env
+from tqdm import tqdm
+
+num_runs = 1
+num_episodes = 300
+
+def run_episode(env, agent):
+    is_terminal = False
+    sum_of_rewards = 0
+    step_count = 0
+
+    obs = env.reset()
+    action = agent.agent_start(obs)
+
+    while not is_terminal:
+        obs, reward, is_terminal, _ = env.step(action)
+        print(agent.steps,end='\r')
+        sum_of_rewards += reward
+        step_count += 1
+        state = obs
+        if step_count == math.inf:
+            agent.agent_end(reward, state, append_buffer=False)
+            break
+        elif is_terminal:
+            agent.agent_end(reward, state, append_buffer=True)
+        else:
+            action, _ = agent.agent_step(reward, state)
+
+    return -step_count
+
+agents = {
+    "Sarsa_tc": SarsaTCAgent,
+    "Tile_Coded_Sarsa_NN": TCSarsaNNAgent,
+}
+
+agent_infos = {
+    "Sarsa_tc": {
+        'step_size': .1,
+        'iht_size': 4096,
+        'num_tilings': 8,
+        'num_tiles': 8,
+    },
+    "Tile_Coded_Sarsa_NN": {
+        'step_size': 1e-4,
+        'num_states': 2,
+        'iht_size': 4096,
+        'num_tilings': 8,
+        'num_tiles': 8,
+    }
+}
+
+metrics = {"ep_rewards": {}, "hyper_params":{}}
+
+def objective(agent_type, hyper_params, num_runs=num_runs):
+    start = time.time()
+
+    ep_rewards = {}
+
+    env_infos = sample_mountaincar_env(42, 2)[1:]
+    algorithm = agent_type + '_' + '_'.join([f'{k}_{v}' for k, v in hyper_params.items()])
+
+    mb = master_bar(env_infos)
+
+    for env_name, env_info in enumerate(mb):
+        if env_name not in ep_rewards:
+            ep_rewards[env_name] = {}
+            print(env_name)
+            ep_rewards[env_name][algorithm] = []
+
+        for run in tqdm(range(num_runs)):
+            agent = agents[agent_type]()
+            env = MountainCarEnv(**env_info)
+            print(env_info)
+            agent_info = {"num_actions": env.action_space.n, "epsilon": 1, "step_size": 0.1, 'discount': .99, 'iht_size': 4096, "seed": run}
+            agent_info.update(agent_infos[agent_type])
+            agent_info.update(hyper_params)
+
+            np.random.seed(run)
+
+            agent.agent_init(agent_info)
+
+            rewards = []
+            epsilon = 1
+
+            for episode in range(num_episodes):
+                print(f"episode {episode}",end='\r')
+                agent.epsilon = epsilon
+                sum_of_rewards = run_episode(env, agent)
+                epsilon *= 0.99
+                rewards.append(sum_of_rewards)
+
+            ep_rewards[env_name].setdefault(algorithm, []).append(rewards)
+
+    end = time.time()
+    print(end - start)
+    metrics['ep_rewards'] = ep_rewards
+    metrics['hyper_params'][algorithm] = hyper_params
+    Path(f"{ROOT_DIR}/experiments/0305/metrics/").mkdir(parents=True, exist_ok=True)
+    torch.save(metrics, f'{ROOT_DIR}/experiments/0305/metrics/{algorithm}.torch')
+    return algorithm, np.mean(metrics["ep_rewards"][env_name][algorithm]), hyper_params
+
+
+if __name__ == '__main__':
+    fire.Fire(objective)
---
+++ experiments/0305/write_jobs.py
@@ -0,0 +1,50 @@
+from itertools import product
+from pathlib import Path
+
+from definitions import ROOT_DIR
+from fastprogress.fastprogress import master_bar, progress_bar
+
+count = 0
+
+def create_job(agent_type, hyper_params):
+    global count
+    cmd = f"python -m experiments.0305.run_episodic --agent_type=\"{agent_type}\" --hyper_params=\"{hyper_params}\""
+    jobs_dir = Path(f"{ROOT_DIR}/experiments/0305/jobs/")
+    jobs_dir.mkdir(parents=True, exist_ok=True)
+    with open(jobs_dir/f"tasks_{count}.sh", 'w') as f:
+        f.write(cmd)
+    print(count, cmd)
+    count += 1
+
+def grid_search(agent_type, param_grid):
+    """grid search for hyperparameter optimization"""
+    param_keys, values = zip(*param_grid.items())
+
+    param_combos = [dict(zip(param_keys, combo)) for combo in product(*values)]
+
+    mb = master_bar(param_combos)
+
+    for i, hyper_params in enumerate(mb):
+        print(hyper_params)
+        create_job(agent_type, hyper_params)
+
+agents = {
+    "Sarsa_tc": None,
+    # "Tile_Coded_Sarsa_NN": None,
+}
+
+params_to_search = {
+    "Sarsa_tc": {
+        'step_size': [1.0, 0.5, 0.25, 0.125, 0.06125],
+        # 'iht_size': [4096],
+        'num_tilings': [4, 8, 16, 32],
+        'num_tiles': [4, 8, 16, 32],
+    },
+}
+
+
+if __name__ == "__main__":
+    for agent_type in master_bar(list(agents.keys())):
+        print(agent_type)
+        grid_search(agent_type, params_to_search[agent_type])
+        print('Jobs: sbatch --array={}-{} ./maze/jobs/run_cpu.sh'.format(0, count-1))
