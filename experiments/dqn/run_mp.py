1dc09920e5f8557c84ccb3328cbea1f088cdf2f9
David Tao <ruoyutao@gmail.com>
2021-04-07 12:04:49 +0800

    settle on sweeps

--- experiments/dqn/run_mp.py
+++ experiments/dqn/run_mp.py
@@ -49,9 +49,9 @@ if __name__ == "__main__":
     pp = PrettyPrinter(indent=4)
     # So here we need to run multiple runs over multiple hyperparams.
 
-    max_replay_sizes = [100, 10000, 50000, 150000]
+    max_replay_sizes = [1000, 25000, 150000]
 
-    update_target_intervals = [100, 10000, 50000, 150000]
+    update_target_intervals = [100, 1000, 5000, 10000]
 
     step_sizes = get_lr()
 
